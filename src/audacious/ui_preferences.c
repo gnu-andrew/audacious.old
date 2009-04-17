27982f042134f048ac47388a1136faf06d3ebcb4
William Pitcock <nenolod@atheme.org>
2009-04-16 20:07:38 -0500

    prefswin_page_destroy(): add missing 'continue' instruction. Found with LLVM static analyzer.

--- src/audacious/ui_preferences.c
+++ src/audacious/ui_preferences.c
@@ -2861,6 +2861,7 @@ prefswin_page_destroy(GtkWidget *container)
         {
             gtk_list_store_remove(GTK_LIST_STORE(model), &iter);
             ret = gtk_tree_model_get_iter_first(model, &iter);
+            continue;
         }
 
         if (index > id)
